2ddd06a038beaa32c0e1f2f68df3641c26f0794b
marckvaldo <marckvaldo@hotmail.com>
2023-02-01 18:38:34 -0300

    Iniciado a base de E2E produtosrepository

---
+++ src/MShop.IntegrationTest/MShop.IntegrationTests/Common/BaseFixture.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MShop.IntegrationTests.Common
+{
+    public abstract class BaseFixture
+    {
+        protected readonly Faker faker;
+        protected static readonly Faker fakerStatic = new Faker("pt_BR");
+        protected BaseFixture()
+        {
+            faker = new Faker("pt_BR"); 
+        }
+    }
+}
---
+++ src/MShop.IntegrationTest/MShop.IntegrationTests/Repository/ProductRepository/ProductRespositoryTesteFixture.cs
@@ -0,0 +1,14 @@
+﻿using MShop.IntegrationTests.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MShop.IntegrationTests.Repository.ProductRepository
+{
+    public class ProductRespositoryTesteFixture : BaseFixture
+    {
+        
+    }
+}
