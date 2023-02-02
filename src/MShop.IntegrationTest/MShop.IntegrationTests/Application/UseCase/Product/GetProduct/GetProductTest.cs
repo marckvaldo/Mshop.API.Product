662909584aa200ef72064bef97ff945a699f28ab
marckvaldo <marckvaldo@hotmail.com>
2023-02-02 18:57:09 -0300

    Implementado o teste SholdReturnErrorWhenCantGetProduct

--- src/MShop.IntegrationTest/MShop.IntegrationTests/Application/UseCase/Product/GetProduct/GetProductTest.cs
+++ src/MShop.IntegrationTest/MShop.IntegrationTests/Application/UseCase/Product/GetProduct/GetProductTest.cs
@@ -10,6 +10,7 @@ using MShop.Repository.Repository;
 using MShop.Repository.Context;
 using ApplicationUseCase = MShop.Application.UseCases.Product.GetProduct;
 using MShop.Business.Validation;
+using MShop.Business.Exception;
 
 namespace MShop.IntegrationTests.Application.UseCase.Product.GetProduct
 {
@@ -43,6 +44,28 @@ namespace MShop.IntegrationTests.Application.UseCase.Product.GetProduct
             Assert.Equal(outPut.CategoryId, productFake.CategoryId);
             Assert.Equal(outPut.Stock, productFake.Stock);
             Assert.Equal(outPut.IsActive, productFake.IsActive);
+
+        }
+
+
+        [Fact(DisplayName = nameof(SholdReturnErrorWhenCantGetProduct))]
+        [Trait("Application-UseCase", "Product Use Case")]
+        public async void SholdReturnErrorWhenCantGetProduct()
+        {
+            RepositoryDbContext dbContext = CreateDBContext();
+
+            var repository = new ProductRepository(dbContext);
+            var notification = new Notifications();
+
+            var productFake = Faker();
+            dbContext.Products.Add(productFake);
+            await dbContext.SaveChangesAsync();
+
+            var useCase = new ApplicationUseCase.GetProduct(repository, notification);
+            var outPut = async () => await useCase.Handle(Guid.NewGuid());
+
+            var exception = await Assert.ThrowsAsync<NotFoundException>(outPut);
+            Assert.Equal(exception.Message, "your search returned null");
 
         }
     }
